7d4dd91b19f064e39b4c73091499b9745543f3d4
Kyle Lacy <kylelacy@kyle.space>
2023-12-08 23:10:52 -0800

    Add a `cargo-generate` template project

---
+++ template/src/main.rs
@@ -0,0 +1,22 @@
+use std::io::Read as _;
+
+fn main() -> eyre::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::builder()
+                .with_default_directive(tracing::level_filters::LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .without_time()
+        .init();
+    color_eyre::install()?;
+
+    let mut stdin = std::io::stdin().lock();
+    let mut input = String::new();
+    stdin.read_to_string(&mut input)?;
+
+    tracing::info!("Hello, world!");
+    println!("{input}");
+
+    Ok(())
+}
